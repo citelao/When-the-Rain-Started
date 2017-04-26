5593e6fb9dc385cafd2a33e43eddb9517a772afc
Ben Stolovitz <ben@stolovitz.com>
2017-04-25 20:41:39 -0500

    look ma WebGL

--- js/Emitter.js
+++ js/Emitter.js
@@ -1,50 +1,53 @@
-function Emitter(num_particles, x, y, width, height) {
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
+function Emitter(o) {
+	this.x = o.x;
+	this.y = o.y;
+	this.width = o.width;
+	this.height = o.height;
+	this.num_particles = o.num_particles;
+	this.texture = o.texture;
 
 	this.particles = [];
+	this.sprites = [];
 
-	this.particle_xs = [];
-	this.particle_ys = [];
+	this.container = new PIXI.Container();
+	o.parent.addChild(this.container);
 
-	this.particle_lifes = [];
-	this.particle_lifetimes = [];
+	for (var i = 0; i < this.num_particles; i++) {
+		this.particles[i] = this.reset_particle();
 
-	for (var i = 0; i < num_particles; i++) {
-		this.reset_particle(i);	
+		this.sprites[i] = new PIXI.Sprite(this.texture);
+		this.sprites[i].anchor.set(0.5, 0.5);
+		this.container.addChild(this.sprites[i]);
 	}
 }
 
-Emitter.prototype.reset_particle = function(i) {
-	this.particle_xs[i] = Math.random() * this.width + this.x;
-	this.particle_ys[i] = Math.random() * this.height + this.y;
-	this.particle_lifes[i] = 0; 
-	this.particle_lifetimes[i] = Math.random() * 10 + 1000;
+Emitter.prototype.reset_particle = function() {
+	return {
+		x: Math.random() * this.width + this.x,
+		y: Math.random() * this.height + this.y,
+		age: 0,
+		lifetime: Math.random() * 400 + 500
+	};
 }
 
 Emitter.prototype.update = function(delta) {
-	for (var i = 0; i < this.particle_xs.length; i++) {
-		this.particle_lifes[i] += delta;
+	for (var i = 0; i < this.particles.length; i++) {
+		var particle = this.particles[i];
+		var sprite = this.sprites[i];
 
-		if(this.particle_lifes[i] > this.particle_lifetimes[i]) {
-			this.reset_particle(i);
+		particle.age += delta;
+		var ease = particle.age / particle.lifetime;
+
+		sprite.x = particle.x;
+		sprite.y = particle.y;
+
+		sprite.scale.x = ease * 1;
+		sprite.scale.y = ease * 1;
+		sprite.alpha = 1 - ease;
+
+		if(particle.age > particle.lifetime) {
+			this.particles[i] = this.reset_particle();	
 		}
-
-		this.particles[i].animate(10).attr({
-			x: this.particle_xs[i],
-			y: this.particle_ys[i]
-		});
 	}
 
-}
-
-Emitter.prototype.draw = function(draw, draw_func) {
-	for (var i = 0; i < this.particle_xs.length; i++) {
-		this.particles[i] = draw_func(draw).attr({
-			x: this.particle_xs[i],
-			y: this.particle_ys[i]
-		});
-	}
 }
--- js/story.js
+++ js/story.js
@@ -1,3 +1,6 @@
+////
+// Generate SVG jitter filters
+////
 function generate_filters(drawable, frames) {
 	var filters = [];
 	for(var i = 0; i < frames; i++) {
@@ -10,50 +13,75 @@ function generate_filters(drawable, frames) {
 	}
 	return filters;
 }
-
-var draw = SVG("drawing");
-var n = draw.nested();
+var draw = SVG('filters');
+var nest = draw.nested();
 
 var FRAMES = 8;
-var JITTER_FILTERS = generate_filters(n, FRAMES);
+var JITTER_FILTERS = generate_filters(nest, FRAMES);
 
-var SCENES = [
-	new Scene_1(n)
-];
 
-// STATE
-var current_scene = false;
-
-function set_scene(index) {
-	current_scene = index;
-	// draw.click(null);
-	draw.click(SCENES[current_scene].click.bind(SCENES[current_scene]));
+////
+// Start PIXI
+////
+var type = "WebGL";
+if(!PIXI.utils.isWebGLSupported()){
+  type = "canvas";
 }
-set_scene(0);
+
+PIXI.utils.sayHello(type);
+
+var app = new PIXI.Application();
+var renderer = PIXI.autoDetectRenderer(256, 256, {
+	resolution: window.devicePixelRatio
+});
+
+document.body.appendChild(renderer.view);
+
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.view.style["margin-left"] = "-20px";
+renderer.view.style["margin-top"] = "-20px";
+renderer.view.style["margin-bottom"] = "-20px";
+renderer.view.style["margin-right"] = "-20px";
+// renderer.autoResize = true;
+renderer.resize(window.innerWidth + 40, window.innerHeight + 40);
 
 // ANIMATE!
 var last = 0; // STATE
 var frame = 0; // STATE
+
+var stage = new PIXI.Container();
+
+// RAINDROP
+var raindrop = new PIXI.Graphics();
+raindrop.lineStyle(4, 0x0033FF, 1);
+raindrop.drawCircle(0, 0, 50);
+
+var rainer = new Emitter({
+	parent: stage,
+	num_particles: 5000,
+	x: 0, 
+	y: 0,
+	width: renderer.view.width,
+	height: renderer.view.width,
+	texture: raindrop.generateCanvasTexture(PIXI.SCALE_MODES.DEFAULT, window.devicePixelRatio)
+});
+
 function animate(time) {
 	var dt = time - last;
-	// 24 FPS b/c we're a movie
+
+	rainer.update(dt);
+
+	renderer.render(stage);
+
+
+	// Change the jitter @ 24FPS
 	if(dt < 1 / 24.1 * 1000) {
 		window.requestAnimationFrame(animate);
 		return;
 	}
-
-	// Update the scene
-	// SCENES[current_scene].update(dt, n);
-
-
-
-	// // Change the jitter
 	frame = (frame + 1) % FRAMES;
-	// n.attr({
-	// 	filter: "url(#" + JITTER_FILTERS[frame].node.id + ")"
-	// });
-	document.getElementById('test').style.filter = "url(#" + JITTER_FILTERS[frame].node.id + ")";
-	console.log(dt);
+	renderer.view.style.filter = "url(#" + JITTER_FILTERS[frame].node.id + ")";
 
 	last = time;
 	window.requestAnimationFrame(animate);
